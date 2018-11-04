a50dab5cbac21cc37d450def5f21c10057f1aecf
gibeulah <gibeulah@microsoft.com>
2018-11-04 22:48:14 +0530

    Add project files.

---
+++ RowWithMax1sInSortedMatrix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RowWithMax1sInSortedMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
