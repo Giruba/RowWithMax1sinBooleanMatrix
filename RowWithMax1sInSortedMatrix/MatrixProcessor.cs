59d5b13c94a054101d779400906f42b2fee32ec4
gibeulah <gibeulah@microsoft.com>
2018-11-05 01:09:50 +0530

    First Commit

---
+++ RowWithMax1sInSortedMatrix/MatrixProcessor.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RowWithMax1sInSortedMatrix
+{
+    class MatrixProcessor
+    {
+        int[,] matrix;
+
+        public MatrixProcessor(int[,] incomingMatrix) {
+            matrix = incomingMatrix;
+        }
+
+        public void GetRowWithMaximumOnes() {
+
+            Console.WriteLine("-----The Input Matrix-----");
+            PrintMatrix(matrix);
+
+            if (isSortedMatrix())
+            {
+                int maxOneRow = FindRowWithMaxOnes();
+                if (maxOneRow != -1)
+                {
+                    Console.WriteLine("Row with the maximum number of ones is " + maxOneRow);
+                }
+                else {
+                    Console.WriteLine("None of the rows have maximum ones!");
+                }
+            }
+            else {
+                Console.WriteLine("Entered matrix is not sorted!");
+            }
+
+        }
+
+        private bool isSortedMatrix() {
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    if (j + 1 < columns)
+                    {
+                        if ((matrix[i, j] == 1) &&(matrix[i, j + 1] == 0))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        
+        private void PrintMatrix(int[,] inputMatrix) {
+            for (int i = 0; i < inputMatrix.GetLength(0); i++) {
+                for (int j = 0; j < inputMatrix.GetLength(1); j++) {
+                    Console.Write(matrix[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------------------------------");
+        }
+
+        private int FindRowWithMaxOnes() {
+            int indexOfOne = 0;
+            int rowWithMax1s = -1;
+            int maxOnes = 0;
+            for (int i = 0; i < matrix.GetLength(1); i++) {
+                if (matrix[0, i] == 1) {
+                    indexOfOne = i;
+                    rowWithMax1s = 0;
+                    maxOnes = matrix.GetLength(1) - indexOfOne;
+                    break;
+                }
+            }
+
+            if (rowWithMax1s == -1) {
+                indexOfOne = matrix.GetLength(1) - 1;
+            }
+
+            for (int i = 1; i < matrix.GetLength(1); i++) {
+                while (matrix[i, indexOfOne] == 1) {
+                    indexOfOne--;
+                }
+                if ((matrix.GetLength(1) - indexOfOne) > maxOnes)
+                {
+                    rowWithMax1s = i;
+                    maxOnes = (matrix.GetLength(1) - indexOfOne);
+                }
+            }
+
+            return rowWithMax1s;
+        }
+    }
+}
--- RowWithMax1sInSortedMatrix/Program.cs
+++ RowWithMax1sInSortedMatrix/Program.cs
@@ -6,7 +6,48 @@ namespace RowWithMax1sInSortedMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Row with maximum ones in sorted boolean matrix!");
+            Console.WriteLine("-----------------------------------------------");
+
+            try
+            {
+                int[,] inputMatrix = GetInput();
+                MatrixProcessor matrixProcessor = new MatrixProcessor(inputMatrix);
+                matrixProcessor.GetRowWithMaximumOnes();
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is"+exception.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static int[,] GetInput() {
+
+            int[,] matrix = null;
+
+            Console.WriteLine("Enter the number of rows in the matrix");
+            try
+            {
+                int rows = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the columns of the matrix");
+                int columns = int.Parse(Console.ReadLine());
+                matrix = new int[rows, columns];
+                for (int i = 0; i < rows; i++) {
+                    Console.WriteLine("Enter the elements of row "+i+" separated by space");
+                    String enteredElements = Console.ReadLine();
+                    String[] elements = enteredElements.Split(' ');
+                    for (int j = 0; j < elements.Length; j++) {
+                        matrix[i, j] = int.Parse(elements[j]);
+                    }
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+
+            return matrix;
         }
     }
 }
